6386b2c6ebc23a3c7572076cd90fae4662512f7b
santyss8 <124742631+santyss8@users.noreply.github.com>
2024-05-17 00:48:53 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const EXCUSA = document.getElementById('excusa');
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function excusa(){
+  let numWho = Math.floor(Math.random() * 4);
+  let numAction = Math.floor(Math.random() * 4);
+  let numWhat = Math.floor(Math.random() * 3);
+  let numWhen = Math.floor(Math.random() * 5);
+  EXCUSA.innerHTML = who[numWho] + ' ' + action[numAction] + ' ' + what[numWhat] + ' ' + when[numWhen];
+}
+
+window.onload = excusa;
